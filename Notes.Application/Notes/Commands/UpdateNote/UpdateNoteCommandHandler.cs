e2bbb02753f50488347def8d4a8be523aff4add8
teperyaemo <rokudenas@bk.ru>
2023-09-26 00:41:27 +0300

    Добавил команды добавления, удаления, изменения

---
+++ Notes.Application/Notes/Commands/UpdateNote/UpdateNoteCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using Notes.Application.Interfaces;
+using Notes.Domain;
+using Microsoft.EntityFrameworkCore;
+using Notes.Application.Common.Exceptions;
+
+namespace Notes.Application.Notes.Commands.UpdateNote
+{
+    public class UpdateNoteCommandHandler
+        : IRequestHandler<UpdateNoteCommand, Unit>
+    {
+        private readonly INotesDbContext _dbContext;
+
+        public UpdateNoteCommandHandler(INotesDbContext dbContext) =>
+            _dbContext = dbContext;
+
+        public async Task<Unit> Handle(UpdateNoteCommand request,
+            CancellationToken cancellationToken)
+        {
+            var entity =
+                await _dbContext.Notes.FirstOrDefaultAsync(note =>
+                    note.Id == request.Id, cancellationToken);
+
+            if (entity == null || entity.UserId != request.UserId)
+            {
+                throw new NotFoundException(nameof(Note), request.Id);
+            }
+
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return Unit.Value;
+        }
+    }
+}
