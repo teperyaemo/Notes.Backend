77ce37106767248870f2f25b1d45607ad94f0f44
teperyaemo <rokudenas@bk.ru>
2023-09-30 23:40:12 +0300

    Добавил сервисы и настроил middleware

--- Notes.Application/DependencyInjection.cs
+++ Notes.Application/DependencyInjection.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.DependencyInjection;
 
 namespace Notes.Application
 {
-    internal class DependencyInjection
+    public static class DependencyInjection
     {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            return services;
+        }
     }
 }
--- Notes.WebApi/Program.cs
+++ Notes.WebApi/Program.cs
@@ -1,6 +1,62 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Notes.Application.Common.Mappings;
+using Notes.Application.Interfaces;
+using Notes.Application;
+using Notes.Persistance;
+using System.Reflection;
 
-app.MapGet("/", () => "Hello World!");
+public class Program
+{
+    private static void Main(string[] args)
+    {        
+        var builder = WebApplication.CreateBuilder(args);
 
-app.Run();
+        builder.Services.AddAutoMapper(config =>
+        {
+            config.AddProfile(new AssemblyMappingProfile(Assembly.GetExecutingAssembly()));
+            config.AddProfile(new AssemblyMappingProfile(typeof(INotesDbContext).Assembly));
+        });
+        builder.Services.AddApplication();
+        builder.Services.AddPersistence(builder.Configuration);
+        builder.Services.AddCors(options =>
+        {
+            options.AddPolicy("AllowAll", policy =>
+            {
+                policy.AllowAnyHeader();
+                policy.AllowAnyMethod();
+                policy.AllowAnyOrigin();
+            });
+        });
+
+        var app = builder.Build();
+
+        using (var scope = app.Services.CreateScope())
+        {
+            var serviceProvider = scope.ServiceProvider;
+
+            try
+            {
+                var context = serviceProvider.GetRequiredService<NotesDbContext>();
+                DbInitializer.Initialize(context);
+            }
+            catch (Exception exception)
+            {
+
+            }
+        }
+
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseRouting();
+        app.UseHttpsRedirection();
+        app.UseCors("AllowAll");
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+
+        app.Run();
+    }
+}
