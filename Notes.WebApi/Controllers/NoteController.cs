12d511baa6c495bb0b3ab703a610614dfff33cb4
teperyaemo <rokudenas@bk.ru>
2023-10-04 23:26:08 +0300

    Добавил базовый контроллер и get контроллер

---
+++ Notes.WebApi/Controllers/NoteController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Notes.Application.Notes.Queries.GetNoteDetails;
+using Notes.Application.Notes.Queries.GetNoteList;
+
+namespace Notes.WebApi.Controllers
+{
+    public class NoteController : BaseController
+    {
+        [HttpGet]
+        public async Task<ActionResult<NoteListVm>> GetAll()
+        {
+            var query = new GetNoteListQuery 
+            { 
+                UserId = UserId
+            };
+            var vm = await Mediator.Send(query);
+            return Ok(vm);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<NoteDetailsVm>> Get(Guid id)
+        {
+            var query = new GetNoteDetailsQuery
+            {
+                UserId = UserId,
+                Id = id
+            };
+            var vm = await Mediator.Send(query);
+            return Ok(vm);
+        }
+    }
+}
