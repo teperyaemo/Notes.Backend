82f7811926b272a4c2f0a7953ef651ca96975aa2
ttteperyaemo <rokudenas@bk.ru>
2023-10-24 22:57:59 +0300

    Добавил тест на плучения множества заметок

---
+++ Notes.Test/Common/QueryTestFixture.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Notes.Application.Common.Mappings;
+using Notes.Application.Interfaces;
+using Notes.Persistance;
+
+namespace Notes.Test.Common
+{
+    public class QueryTestFixture : IDisposable
+    {
+        public NotesDbContext context;
+        public IMapper mapper;
+
+        public QueryTestFixture()
+        {
+            context = NotesContextFactory.Create();
+            var configurationProvider = new MapperConfiguration(config =>
+            {
+                config.AddProfile(new AssemblyMappingProfile(
+                    typeof(INotesDbContext).Assembly));
+            });
+            mapper = configurationProvider.CreateMapper();
+        }
+
+        public void Dispose()
+        {
+            NotesContextFactory.Destroy(context);
+        }
+    }
+
+    [CollectionDefinition("QueryCollection")]
+    public class QueryCollection : ICollectionFixture<QueryTestFixture> { }
+}
---
+++ Notes.Test/Notes/Queries/GetNoteListQueryHandlerTests.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using Notes.Application.Notes.Queries.GetNoteDetails;
+using Notes.Application.Notes.Queries.GetNoteList;
+using Notes.Persistance;
+using Notes.Test.Common;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Test.Notes.Queries
+{
+    public class GetNoteListQueryHandlerTests
+    {
+        private readonly NotesDbContext context;
+        private readonly IMapper mapper;
+
+        public GetNoteListQueryHandlerTests(QueryTestFixture fixture)
+        {
+            context = fixture.context;
+            mapper = fixture.mapper;
+        }
+
+        [Fact]
+        public async void GetNoteListQueryHandler_Success()
+        {
+            //Arrange
+            var handler = new GetNoteListQueryHandler(context, mapper);
+
+            //Act
+            var result = await handler.Handle(
+                new GetNoteListQuery
+                {
+                    UserId = NotesContextFactory.UserBId
+                },
+                CancellationToken.None);
+
+            //Assert
+            result.ShouldBeOfType<NoteListVm>();
+            result.Notes.Count.ShouldBe(2);
+        }
+    }
+}
